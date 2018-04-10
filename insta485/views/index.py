a92c481994551235dc2e8f47ea97f453984cb979
leolurunhe <leolu1996@outlook.com>
2018-04-09 21:21:17 -0400

    Update index.py

--- insta485/views/index.py
+++ insta485/views/index.py
@@ -5,5 +5,5 @@ import insta485
 
 @insta485.app.route('/views/', methods=["GET","POST"])
 def hello():
-    ret=flask.jsonify({"value" : "Hello World."})
+    ret=flask.jsonify({"value" : "Hello World"})
     return ret
