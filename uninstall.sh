bed1f777a6a62b72707dcea54d0ddbebb22fdb32
leolurunhe <leolu1996@outlook.com>
2018-05-07 22:45:50 -0400

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,16 @@
+cd extensions
+jupyter nbextension uninstall cell.js --user
+jupyter nbextension uninstall codecell.js --user
+jupyter nbextension uninstall debug.js --user
+jupyter nbextension uninstall deletecell.js --user
+jupyter nbextension uninstall disablenotebookextensions.js --user
+jupyter nbextension uninstall importdataset.js --user
+jupyter nbextension uninstall insertcell.js --user
+jupyter nbextension uninstall newdataset.js --user
+jupyter nbextension uninstall newpart.js --user
+jupyter nbextension uninstall newquestion.js --user
+jupyter nbextension uninstall submitdataset.js --user
+jupyter nbextension uninstall submitnotebook.js --user
+jupyter nbextension uninstall textcell.js --user
+
+
